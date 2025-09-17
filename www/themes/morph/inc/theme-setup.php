cba416a593fb232b2c51506a842b4708c181ed8a
Arthur Satarov <asv.webdev@gmail.com>
2025-09-17 21:09:14 +0500

    Добавил поддержку базовых функций WordPress

--- www/themes/morph/inc/theme-setup.php
+++ www/themes/morph/inc/theme-setup.php
@@ -5,6 +5,7 @@
  * @package MORPH
  */
 
+add_action( 'after_setup_theme', 'morph_theme_support' );
 add_action( 'after_setup_theme', 'morph_register_theme_menus' );
 add_action( 'wp_enqueue_scripts', 'morph_enqueue_assets' );
 
@@ -28,3 +29,34 @@ function morph_enqueue_assets() {
 	wp_enqueue_script( 'morph', MORPH_THEME_URI . '/assets/scripts/main.js', array(), MORPH_THEME_VERSION, true );
 }
 
+/* THEME SUPPORT
+/––––––––––––––––––––––––*/
+// registers support for WordPress features
+function morph_theme_support() {
+	// allow WordPress to manage the <title> tag automatically.
+	// => https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+	add_theme_support( 'title-tag' );
+
+	// enable automatic RSS feed links in <head>.
+	// => https://developer.wordpress.org/reference/functions/add_theme_support/#automatic-feed-links
+	add_theme_support( 'automatic-feed-links' );
+
+	// enable featured images (post thumbnails) for posts and pages.
+	// => https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	add_theme_support( 'post-thumbnails' );
+
+	// use valid HTML5 markup for various components.
+	// => https://developer.wordpress.org/reference/functions/add_theme_support/#html5
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+}

2b3944c7422c9779097a259047864c05696419a8
Arthur Satarov <asv.webdev@gmail.com>
2025-09-17 21:07:13 +0500

    Добавил функцию подключения скриптов и стилей

--- www/themes/morph/inc/theme-setup.php
+++ www/themes/morph/inc/theme-setup.php
@@ -6,6 +6,7 @@
  */
 
 add_action( 'after_setup_theme', 'morph_register_theme_menus' );
+add_action( 'wp_enqueue_scripts', 'morph_enqueue_assets' );
 
 /* REGISTER MENUS
 /––––––––––––––––––––––––*/
@@ -15,5 +16,15 @@ function morph_register_theme_menus() {
 			'main_menu' => esc_html__( 'Main menu', 'morph' ),
 		)
 	);
+}
+
+/* ENQUEUE STYLES/SCRIPTS
+/––––––––––––––––––––––––*/
+function morph_enqueue_assets() {
+	// styles
+	wp_enqueue_style( 'morph', MORPH_THEME_URI . '/assets/styles/main.css', array(), MORPH_THEME_VERSION, 'all' );
+
+	// scripts
+	wp_enqueue_script( 'morph', MORPH_THEME_URI . '/assets/scripts/main.js', array(), MORPH_THEME_VERSION, true );
 }
 

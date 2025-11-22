3724cf82e9540ff43dc21bf7fd6e66114170f685
Arthur Satarov <asv.webdev@gmail.com>
2025-11-22 19:00:15 +0500

    Добавил функцию для очистки номера телефона
    Возвращает очищенный номер для tel: ссылки

---
+++ www/themes/morph/inc/functions/get-sanitized-phone.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Cleans the phone number for use in href="tel:"
+ * Removes all characters except numbers and +
+ *
+ * @param string $phone Phone number in any format
+ * @return string Cleaned number for tel: links
+ */
+function morph_get_sanitized_phone($phone) {
+    return preg_replace('/[^0-9+]/', '', $phone);
+}

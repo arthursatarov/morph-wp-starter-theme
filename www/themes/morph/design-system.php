baeff92a73ebafd0f19fee0df9ce20bd0966e95c
Arthur Satarov <asv.webdev@gmail.com>
2025-11-28 13:19:33 +0500

    Улучшил глобальные CSS стили

--- www/themes/morph/design-system.php
+++ www/themes/morph/design-system.php
@@ -13,377 +13,126 @@ get_header();
 
 <div class="wrapper region">
 	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Buttons / Tooltips / Dropdowns</h3>
-		<div class="cluster" style="--cluster-gap: 4rem; align-items: start;">
-			<div class="stack">
-				<p>Button icon</p>
-				<button
-					class="btn btn--icon"
-					data-tooltip-target="tooltip-1"
-					aria-label="Add file">
-					<?php morph_print_sprite_icon( [
-						'icon' => 'add',
-						'class' => 'btn__icon'
-					] ); ?>
-				</button>
-				<?php morph_print_tooltip( 'tooltip-1', 'Add file' ); ?>
-			</div>
-			<div class="stack">
-				<div class="stack">
-					<p>Button</p>
-					<div class="cluster">
-						<button class="btn btn--primary">
-							<span class="btn__label">Button</span>
-						</button>
-						<button class="btn">
-							<span class="btn__label">Button</span>
-						</button>
-						<button class="btn" disabled>
-							<span class="btn__label">Button</span>
-						</button>
-					</div>
-				</div>
-				<div class="stack">
-					<p>Button with icon</p>
-					<div class="cluster">
-						<button class="btn btn--primary">
-							<?php morph_print_sprite_icon( [
-								'icon' => 'add',
-								'class' => 'btn__icon'
-							] ); ?>
-							<span class="btn__label">Button</span>
-						</button>
-						<button class="btn">
-							<?php morph_print_sprite_icon( [
-								'icon' => 'add',
-								'class' => 'btn__icon'
-							] ); ?>
-							<span class="btn__label">Button</span>
-						</button>
-						<button class="btn" disabled>
-							<?php morph_print_sprite_icon( [
-								'icon' => 'add',
-								'class' => 'btn__icon'
-							] ); ?>
-							<span class="btn__label">Button</span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div class="stack">
-				<p>Dropdown</p>
-				<button
-					class="btn"
-					data-dropdown-target="dropdown-1">
-					<span class="btn__label">Dropdown</span>
-					<?php morph_print_sprite_icon( [
-						'icon' => 'chevron-down',
-						'class' => 'btn__icon'
-					] ); ?>
-				</button>
-				<div id="dropdown-1" class="dropdown">
-					<ul role="list">
-						<li>Item 1</li>
-						<li>Item 2</li>
-						<li>Item 3</li>
-						<li>Item 4</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+		<article class="prose">
+			<h1>Современные подходы к веб-разработке</h1>
 
-<div class="wrapper region">
-	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Tags</h3>
-		<div class="cluster">
-			<a class="tag tag--primary">
-				<?php morph_print_sprite_icon( [
-					'icon' => 'globe',
-					'class' => 'tag__icon'
-				] ); ?>
-				<span class="tag__label">Tag</span>
-			</a>
-			<a class="tag">
-				<?php morph_print_sprite_icon( [
-					'icon' => 'globe',
-					'class' => 'tag__icon'
-				] ); ?>
-				<span class="tag__label">Tag</span>
-			</a>
-		</div>
-	</div>
-</div>
+			<p>Веб-разработка прошла долгий путь от простых статических страниц до сложных интерактивных приложений. В этой статье мы рассмотрим ключевые принципы и практики, которые помогут создавать качественные и доступные веб-сайты.</p>
 
-<div class="wrapper region">
-	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Accordion</h3>
-		<div class="accordion" data-accordion="collapse" style="max-inline-size: 40rem;">
-			<div class="accordion__item">
-				<h2>
-					<button class="accordion__item-trigger">
-						<span>Section 1</span>
-						<?php morph_print_sprite_icon( [
-							'icon' => 'chevron-right',
-							'class' => 'accordion__item-icon'
-						] ); ?>
-					</button>
-				</h2>
-				<div class="accordion__item-content">
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, numquam quae minima beatae molestiae recusandae cupiditate ea tempore a nulla sequi ex delectus dolorem neque?</p>
-				</div>
-			</div>
-			<div class="accordion__item">
-				<h2>
-					<button class="accordion__item-trigger">
-						<span>Section 2</span>
-						<?php morph_print_sprite_icon( [
-							'icon' => 'chevron-right',
-							'class' => 'accordion__item-icon'
-						] ); ?>
-					</button>
-				</h2>
-				<div class="accordion__item-content">
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, numquam quae minima beatae molestiae recusandae cupiditate ea tempore a nulla sequi ex delectus dolorem neque?</p>
-				</div>
-			</div>
-			<div class="accordion__item">
-				<h2>
-					<button class="accordion__item-trigger">
-						<span>Section 3</span>
-						<?php morph_print_sprite_icon( [
-							'icon' => 'chevron-right',
-							'class' => 'accordion__item-icon'
-						] ); ?>
-					</button>
-				</h2>
-				<div class="accordion__item-content">
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, numquam quae minima beatae molestiae recusandae cupiditate ea tempore a nulla sequi ex delectus dolorem neque?</p>
-				</div>
-			</div>
-			<div class="accordion__item">
-				<h2>
-					<button class="accordion__item-trigger">
-						<span>Section 4</span>
-						<?php morph_print_sprite_icon( [
-							'icon' => 'chevron-right',
-							'class' => 'accordion__item-icon'
-						] ); ?>
-					</button>
-				</h2>
-				<div class="accordion__item-content">
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, numquam quae minima beatae molestiae recusandae cupiditate ea tempore a nulla sequi ex delectus dolorem neque?</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+			<h2>Основы типографики в вебе</h2>
 
-<div class="wrapper region">
-	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Tabs</h3>
-		<div class="tabs" style="max-inline-size: 40rem;" data-tabs>
-			<div class="tabs__list" role="tablist" aria-label="Примеры табов">
-				<button
-					class="tabs__trigger"
-					role="tab"
-					aria-selected="true"
-					aria-controls="tabs-panel-1"
-					id="tabs-tab-1"
-					data-tabs-trigger
-				>
-					<?php morph_print_sprite_icon( [
-						'icon' => 'globe',
-						'class' => 'tabs__trigger-icon'
-					] ); ?>
-					<span class="tabs__trigger-label">Profile</span>
-				</button>
-				<button
-					class="tabs__trigger"
-					role="tab"
-					aria-selected="false"
-					aria-controls="tabs-panel-2"
-					id="tabs-tab-2"
-					data-tabs-trigger
-				>
-					<?php morph_print_sprite_icon( [
-						'icon' => 'globe',
-						'class' => 'tabs__trigger-icon'
-					] ); ?>
-					<span class="tabs__trigger-label">Dashboard</span>
-				</button>
-				<button
-					class="tabs__trigger"
-					role="tab"
-					aria-selected="false"
-					aria-controls="tabs-panel-3"
-					id="tabs-tab-3"
-					data-tabs-trigger
-				>
-					<?php morph_print_sprite_icon( [
-						'icon' => 'globe',
-						'class' => 'tabs__trigger-icon'
-					] ); ?>
-					<span class="tabs__trigger-label">Settings</span>
-				</button>
-				<button
-					class="tabs__trigger"
-					role="tab"
-					aria-selected="false"
-					aria-controls="tabs-panel-3"
-					id="tabs-tab-4"
-					data-tabs-trigger
-					disabled
-				>
-					<?php morph_print_sprite_icon( [
-						'icon' => 'globe',
-						'class' => 'tabs__trigger-icon'
-					] ); ?>
-					<span class="tabs__trigger-label">Overview</span>
-				</button>
-			</div>
-			<div class="tabs__panels">
-				<div
-					class="tabs__panel"
-					role="tabpanel"
-					id="tabs-panel-1"
-					aria-labelledby="tabs-tab-1"
-					data-tabs-panel
-				>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam saepe esse veniam in omnis, laudantium voluptatem vero. Assumenda, qui cumque quisquam totam debitis itaque at, quam sit quia animi sint?</p>
-				</div>
+			<p>Типографика — это искусство и техника оформления текста. Хорошая типографика делает контент более читаемым и приятным для восприятия. <strong>Правильный выбор шрифтов, размеров и отступов</strong> критически важен для пользовательского опыта.</p>
 
-				<div
-					class="tabs__panel"
-					role="tabpanel"
-					id="tabs-panel-2"
-					aria-labelledby="tabs-tab-2"
-					hidden
-					data-tabs-panel
-				>
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio magnam necessitatibus ducimus, voluptates neque ullam nemo. Rerum accusamus consectetur recusandae in necessitatibus! Ratione, ducimus sit?</p>
-				</div>
+			<p>Существует несколько ключевых аспектов, которые необходимо учитывать при работе с типографикой:</p>
 
-				<div
-					class="tabs__panel"
-					role="tabpanel"
-					id="tabs-panel-3"
-					aria-labelledby="tabs-tab-3"
-					hidden
-					data-tabs-panel
-				>
-					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum dolores quisquam velit sint perferendis exercitationem, neque alias itaque consequuntur non?</p>
-				</div>
-				<div
-					class="tabs__panel"
-					role="tabpanel"
-					id="tabs-panel-4"
-					aria-labelledby="tabs-tab-4"
-					hidden
-					data-tabs-panel
-				>
-					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum dolores quisquam velit sint perferendis exercitationem, neque alias itaque consequuntur non?</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+			<ul>
+				<li>Размер шрифта должен быть достаточно большим для комфортного чтения (минимум 16px для основного текста)</li>
+				<li>Межстрочный интервал влияет на удобство чтения длинных текстов</li>
+				<li>Оптимальная длина строки составляет 60-75 символов</li>
+				<li>Контрастность текста и фона должна соответствовать стандартам <em>WCAG 2.1</em></li>
+			</ul>
 
-<div class="wrapper region">
-	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Tables</h3>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Место</th>
-					<th>Оценка</th>
-					<th>Название фильма</th>
-					<th>Год выхода</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>1</td>
-					<td>9.1</td>
-					<td>Зелёная миля</td>
-					<td>1999</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td>9.1</td>
-					<td>Побег из Шоушенка</td>
-					<td>1994</td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td>8.6</td>
-					<td>Властелин колец: Возвращение Короля</td>
-					<td>2003</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</div>
+			<h3>Выбор шрифтов</h3>
 
-<div class="wrapper region">
-	<div class="stack" style="--stack-space: 2rem;">
-		<h3>Modal / Drawer</h3>
-		<div class="cluster">
-			<button
-				class="btn"
-				data-modal-target="modal-1"
-				aria-haspopup="dialog">
-				<span class="btn__label">Open Modal</span>
-			</button>
-			<button
-				class="btn"
-				data-drawer-target="drawer-1"
-				aria-label="Open Drawer"
-				aria-expanded="false"
-				aria-controls="drawer-1">
-				<span class="btn__label">Open Drawer</span>
-			</button>
-		</div>
-	</div>
-	<div
-		id="modal-1"
-		class="modal"
-		data-modal-state="hidden"
-		data-modal-backdrop="static">
-		<div class="modal__backdrop"></div>
-		<div class="modal__wrapper">
-			<button
-				class="modal__close btn btn--icon"
-				data-modal-hide="modal-1">
-				<?php morph_print_sprite_icon( [
-					'icon' => 'dismiss',
-					'class' => 'btn__icon'
-				] ); ?>
-			</button>
-			<div class="stack">
-				<h4>Modal Heading</h4>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum a maiores modi vitae fugit, facere perspiciatis dicta voluptatibus, veniam, repudiandae vero suscipit in. Laudantium maxime hic sapiente est dolor?</p>
-			</div>
-		</div>
-	</div>
-	<div id="drawer-1" class="drawer">
-		<div class="drawer__backdrop"></div>
-		<div class="drawer__wrapper">
-			<button
-				class="modal__close btn btn--icon"
-				data-drawer-hide="drawer-1"
-				aria-label="Close Drawer">
-				<?php morph_print_sprite_icon( [
-					'icon' => 'dismiss',
-					'class' => 'btn__icon'
-				] ); ?>
-			</button>
-			<div class="stack">
-				<h4>Drawer</h4>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor, eius officiis dolorum, numquam sit in iure cum at aliquid nulla deserunt? Aut minus molestias ipsam cum ab laborum dolores.</p>
-			</div>
-		</div>
+			<p>При выборе шрифтов для веб-проекта следует учитывать не только эстетику, но и практичность. Веб-безопасные шрифты гарантируют корректное отображение на всех устройствах, в то время как кастомные шрифты позволяют создать уникальный визуальный стиль.</p>
+
+			<blockquote>
+				<p>Типографика — это то, что стоит между читателем и информацией. Хорошая типографика незаметна, плохая — раздражает.</p>
+				<cite>Роберт Брингхерст</cite>
+			</blockquote>
+
+			<h3>Иерархия заголовков</h3>
+
+			<p>Правильная иерархия заголовков помогает структурировать контент и улучшает как визуальное восприятие, так и доступность для скринридеров.</p>
+
+			<h4>Практические рекомендации</h4>
+
+			<p>Используйте заголовки последовательно, не пропуская уровни. Например, после <code>h2</code> должен идти <code>h3</code>, а не <code>h4</code>. Это важно для семантической структуры документа.</p>
+
+			<ol>
+				<li>Всегда начинайте страницу с <code>h1</code></li>
+				<li>Используйте только один <code>h1</code> на странице</li>
+				<li>Не выбирайте заголовки по внешнему виду — используйте CSS для стилизации</li>
+				<li>Каждый заголовок должен быть осмысленным и описательным</li>
+			</ol>
+
+			<h2>Работа с цветом и контрастностью</h2>
+
+			<p>Цвет играет важную роль в веб-дизайне, но его использование должно быть продуманным. Недостаточный контраст между текстом и фоном может сделать контент нечитаемым для людей с нарушениями зрения.</p>
+
+			<h3>Стандарты WCAG</h3>
+
+			<p>Web Content Accessibility Guidelines (WCAG) устанавливают минимальные требования к контрастности:</p>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Уровень</th>
+						<th>Обычный текст</th>
+						<th>Крупный текст</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>AA</td>
+						<td>4.5:1</td>
+						<td>3:1</td>
+					</tr>
+					<tr>
+						<td>AAA</td>
+						<td>7:1</td>
+						<td>4.5:1</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<h3>Выбор цветовой палитры</h3>
+
+			<p>При создании цветовой схемы важно учитывать не только эстетические предпочтения, но и психологическое воздействие цветов, а также их влияние на читаемость текста.</p>
+
+			<h4>Основные принципы</h4>
+
+			<p>Выбирайте цвета осознанно. Каждый цвет должен иметь свою функцию в дизайн-системе. Например, красный традиционно используется для ошибок, зелёный — для успешных действий, синий — для информационных сообщений.</p>
+
+			<h2>Адаптивная типографика</h2>
+
+			<p>Современные веб-сайты должны корректно отображаться на устройствах с различными размерами экранов. <mark>Адаптивная типографика</mark> позволяет тексту масштабироваться плавно в зависимости от ширины вьюпорта.</p>
+
+			<pre><code>/* Пример использования clamp() */
+.text {
+  font-size: clamp(1rem, 0.95rem + 0.25vw, 1.125rem);
+}</code></pre>
+
+			<p>Функция <code>clamp()</code> принимает три параметра: минимальное значение, предпочтительное значение и максимальное значение. Это позволяет создавать плавно масштабируемые размеры шрифтов.</p>
+
+			<h3>Viewport-based units</h3>
+
+			<p>Использование единиц измерения на основе вьюпорта (<code>vw</code>, <code>vh</code>, <code>vmin</code>, <code>vmax</code>) даёт дополнительную гибкость, но требует осторожности, чтобы избежать слишком мелкого или крупного текста на экстремальных размерах экранов.</p>
+
+			<hr>
+
+			<h2>Заключение</h2>
+
+			<p>Качественная типографика — это фундамент хорошего веб-дизайна. Она требует внимания к деталям, понимания принципов читаемости и доступности, а также технических знаний для корректной реализации.</p>
+
+			<p>Инвестируя время в правильную настройку типографики, вы создаёте основу для приятного и эффективного взаимодействия пользователей с вашим контентом. Помните: <strong>хорошая типографика незаметна, но её отсутствие всегда бросается в глаза</strong>.</p>
+
+			<figure>
+				<img src="https://placehold.co/800x400/" alt="Пример типографики"
+					width="800" height="400">
+				<figcaption>Визуальная иерархия создаётся через размер, вес и цвет шрифта</figcaption>
+			</figure>
+
+			<h3>Дополнительные ресурсы</h3>
+
+			<p>Для углублённого изучения темы рекомендуем следующие ресурсы:</p>
+
+			<ul>
+				<li><a href="#">Practical Typography by Matthew Butterick</a> — исчерпывающее руководство по типографике</li>
+				<li><a href="#">Web Typography by Richard Rutter</a> — специализированная книга о типографике в вебе</li>
+				<li><a href="#">Type Scale</a> — инструмент для создания типографических систем</li>
+				<li><a href="#">WCAG Guidelines</a> — официальная документация по доступности</li>
+			</ul>
+		</article>
 	</div>
 </div>
 

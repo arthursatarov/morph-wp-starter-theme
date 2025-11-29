c5fadd7824a666792fac169c4ef8b0e74b39c92d
Arthur Satarov <asv.webdev@gmail.com>
2025-11-29 12:04:36 +0500

    Фикс бага при закрытии drawer и modal

--- www/themes/morph/src/scripts/components/drawer.js
+++ www/themes/morph/src/scripts/components/drawer.js
@@ -341,26 +341,14 @@ class DrawerManager {
    * Lock body scroll
    */
   lockBodyScroll() {
-    const scrollY = window.scrollY;
-
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-    document.body.style.width = '100%';
+		document.documentElement.style.overflow = 'hidden';
   }
 
   /**
    * Unlock body scroll
    */
   unlockBodyScroll() {
-    const scrollY = document.body.style.top;
-
-    document.body.style.position = '';
-    document.body.style.top = '';
-    document.body.style.width = '';
-
-    if (scrollY) {
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
+		document.documentElement.style.overflow = '';
   }
 
   /**
--- www/themes/morph/src/scripts/components/modal.js
+++ www/themes/morph/src/scripts/components/modal.js
@@ -346,28 +346,14 @@ class ModalManager {
    * Lock body scroll
    */
   lockBodyScroll() {
-    // Store current scroll position
-    const scrollY = window.scrollY;
-
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-    document.body.style.width = '100%';
+		document.documentElement.style.overflow = 'hidden';
   }
 
   /**
    * Unlock body scroll
    */
   unlockBodyScroll() {
-    // Restore scroll position
-    const scrollY = document.body.style.top;
-
-    document.body.style.position = '';
-    document.body.style.top = '';
-    document.body.style.width = '';
-
-    if (scrollY) {
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
+		document.documentElement.style.overflow = '';
   }
 
   /**

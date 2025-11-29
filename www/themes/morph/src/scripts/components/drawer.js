664e62a003c03291c9ecc62890a14e366831a9d8
Arthur Satarov <asv.webdev@gmail.com>
2025-11-29 21:33:09 +0500

    Опять фикс скролла

--- www/themes/morph/src/scripts/components/drawer.js
+++ www/themes/morph/src/scripts/components/drawer.js
@@ -209,24 +209,14 @@ export function initDrawers() {
  * Lock body scroll
  */
 function lockBodyScroll() {
-	const scrollY = window.scrollY;
-	document.body.style.position = 'fixed';
-	document.body.style.top = `-${scrollY}px`;
-	document.body.style.width = '100%';
+	document.documentElement.style.overflow = 'hidden';
 }
 
 /**
  * Unlock body scroll
  */
 function unlockBodyScroll() {
-	const scrollY = document.body.style.top;
-	document.body.style.position = '';
-	document.body.style.top = '';
-	document.body.style.width = '';
-
-	if (scrollY) {
-		window.scrollTo(0, parseInt(scrollY || '0') * -1);
-	}
+	document.documentElement.style.overflow = '';
 }
 
 /**
--- www/themes/morph/src/scripts/components/modal.js
+++ www/themes/morph/src/scripts/components/modal.js
@@ -242,24 +242,14 @@ function setupFocusTrap(modal) {
  * Lock body scroll
  */
 function lockBodyScroll() {
-	const scrollY = window.scrollY;
-	document.body.style.position = 'fixed';
-	document.body.style.top = `-${scrollY}px`;
-	document.body.style.width = '100%';
+	document.documentElement.style.overflow = 'hidden';
 }
 
 /**
  * Unlock body scroll
  */
 function unlockBodyScroll() {
-	const scrollY = document.body.style.top;
-	document.body.style.position = '';
-	document.body.style.top = '';
-	document.body.style.width = '';
-
-	if (scrollY) {
-		window.scrollTo(0, parseInt(scrollY || '0') * -1);
-	}
+	document.documentElement.style.overflow = '';
 }
 
 /**
